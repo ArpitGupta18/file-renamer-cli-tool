276a098d252dfec4b847b788befd5013930f6755
Arpit Gupta <gupta.14arpit@gmail.com>
2026-02-17 19:46:04 +0545

    refactor: moved function call into fileService; maintain code

--- index.js
+++ index.js
@@ -9,50 +9,50 @@ const {
 } = require("./services/fileService");
 const { default: chalk } = require("chalk");
 
-async function main() {
-	program
-		.option("--folder <path>", "Folder path")
-		.option("--pattern <pattern>", "Rename pattern")
-		.option("--start <number>", "Starting number", 1)
-		.option("--ext <extension>", "File extension filter");
+program
+	.option("--folder <path>", "Folder path")
+	.option("--pattern <pattern>", "Rename pattern")
+	.option("--start <number>", "Starting number", 1)
+	.option("--ext <extension>", "File extension filter");
 
-	program.parse(process.argv);
+program.parse(process.argv);
 
-	const options = program.opts();
+const options = program.opts();
 
-	if (!options.folder || !options.pattern) {
-		console.log("Folder and pattern are required");
-		process.exit(1);
-	}
+if (!options.folder || !options.pattern) {
+	console.log("Folder and pattern are required");
+	process.exit(1);
+}
 
-	const folder = options.folder;
+const folder = options.folder;
+const extension = options.ext ?? null;
+const pattern = options.pattern;
+const startNumber = Number(options.start) ?? 1;
 
-	const folderExists = await checkDirectoryExist(folder);
-
-	if (!folderExists) {
-		console.log(chalk.redBright("Directory doesn't exist"));
+function displayPreview(renameMap) {
+	if (renameMap.length === 0) {
+		console.log(chalk.yellow("No files to rename."));
 		return;
 	}
 
-	const files = await getDirectoryFileNames(folder);
-	// console.log(files);
+	renameMap.forEach(({ oldName, newName }) => {
+		console.log(chalk.bgYellow(oldName) + " → " + chalk.bgGreen(newName));
+	});
+}
+async function main() {
+	try {
+		const renameMap = await prepareRename(
+			folder,
+			pattern,
+			startNumber,
+			extension,
+		);
 
-	const extension = options.ext ?? null;
-
-	const filesWithSpecificExtension = !!extension
-		? await getFilesWithSpecificExtension(files, extension)
-		: files;
-
-	const pattern = options.pattern;
-	const startNumber = options.start ?? 1;
-
-	const generatedFileNames = await prepareRename(
-		filesWithSpecificExtension,
-		pattern,
-		startNumber,
-	);
-
-	console.log(generatedFileNames);
+		displayPreview(renameMap);
+	} catch (error) {
+		console.log(chalk.redBright(error.message));
+		process.exit(1);
+	}
 }
 
 main();
--- services/fileService.js
+++ services/fileService.js
@@ -21,22 +21,31 @@ async function getDirectoryFileNames(directory) {
 }
 
 async function getFilesWithSpecificExtension(files, extension) {
-	return files
-		.filter(
-			(file) =>
-				path.extname(file).split(".")[1].toLowerCase() ===
-				extension.toLowerCase(),
-		)
-		.sort();
+	return files.filter(
+		(file) =>
+			path.extname(file).split(".")[1].toLowerCase() ===
+			extension.toLowerCase(),
+	);
 }
 
-async function prepareRename(files, pattern, startNumber) {
+async function prepareRename(folder, pattern, startNumber, ext) {
+	const folderExists = await checkDirectoryExist(folder);
+
+	if (!folderExists) {
+		throw new Error("Directory doesn't exist");
+	}
+
+	let files = await getDirectoryFileNames(folder);
+
+	if (ext) {
+		files = await getFilesWithSpecificExtension(files, ext);
+	}
+
+	files.sort();
+
 	return generateNewNames(files, pattern, startNumber);
 }
 
 module.exports = {
-	checkDirectoryExist,
-	getDirectoryFileNames,
-	getFilesWithSpecificExtension,
 	prepareRename,
 };
